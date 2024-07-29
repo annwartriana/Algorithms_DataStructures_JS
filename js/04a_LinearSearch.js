b773954514ddb6fdb07553b38ddc5d2fb0d8debb
Annwar Triana <annwartriana@gmail.com>
2024-07-29 09:46:58 -0500

    beggining searching algorithms

---
+++ js/04a_LinearSearch.js
@@ -0,0 +1,13 @@
+function linearSearch(array, numSearched) {
+  for (index in array) {
+    if (numSearched == array[index]) return index;
+  }
+  return -1;
+}
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4))
+// console.log(linearSearch([10, 15, 20, 25, 30], 15)) // 1
+// console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4)) // 5
+// console.log(linearSearch([100], 100)) // 0
+// console.log(linearSearch([1,2,3,4,5], 6)) // -1
+// console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10)) // -1
+// console.log(linearSearch([100], 200)) // -1
